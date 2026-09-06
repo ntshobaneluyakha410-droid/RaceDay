1573b1a6ee7ac3469b358f24dd768bb8baaad60e
ntshobaneluyakha410-droid <ntshobaneluyakha410@gmail.com>
2026-09-06 21:08:46 +0200

    Add files via upload

---
+++ docs/RaceDayDB.sql
@@ -0,0 +1,13 @@
+SELECT COUNT(*) AS UserCount FROM Users;
+
+SELECT COUNT(*) AS OrganiserCount FROM Organisers;
+
+SELECT COUNT(*) AS ParticipantCount FROM Participants;
+
+SELECT COUNT(*) AS EventCount FROM Events;
+
+SELECT COUNT(*) AS CategoryCount FROM Categories;
+
+SELECT COUNT(*) AS EnrolmentCount FROM Enrolments;
+
+SELECT COUNT(*) AS ResultCount FROM Results;
